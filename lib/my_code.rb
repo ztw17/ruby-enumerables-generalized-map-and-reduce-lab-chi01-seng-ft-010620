b088b376071afcb1be7902aa0094cacec4782f90
Zach Weber <weberzt@gmail.com>
2019-12-17 00:57:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1 
+  end
+  new
+end
+
+def reduce(array, sv=nil)
+  if 
+end
